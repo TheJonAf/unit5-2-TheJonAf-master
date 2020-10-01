ad30ddedbc3e39e1bda0e2df40595df561a9598c
TheJonAf <70059704+TheJonAf@users.noreply.github.com>
2020-09-30 21:40:10 -0400

    Average Calculatior assignment

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Threading;
+using System.Transactions;
 
 namespace ITSS131
 {
@@ -6,8 +8,32 @@ namespace ITSS131
     {
         public static void Main(string[] args)
         {
-            Console.Write("How many numbers would you like to add?"  );
-            Console.ReadLine();
+
+
+            Console.Write("How many numbers would you like to add?  ");
+            int field = Convert.ToInt32(Console.ReadLine());
+
+            double[] sumNumber = new double[field];
+            double x = 0;
+
+            for (int c = 0; c < field; c++)
+            {
+                Console.Write("Enter a number:  ");
+                sumNumber[c] = Convert.ToDouble(Console.ReadLine());
+            }
+            foreach (double d in sumNumber)
+            {
+                x += d;
+            }
+
+            double avg = x / field;
+            
+            Console.WriteLine("Average: {0}", avg);
+
         }
+
+
+
+
     }
 }
